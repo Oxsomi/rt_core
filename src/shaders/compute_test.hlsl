d3753cd706d16b69a3e19d1e20e59d4c08767667
Nielsbishere <n@osomi.net>
2023-10-04 22:01:47 +0200

    Fixed issue because swapchain is unorm, not float

--- src/shaders/compute_test.hlsl
+++ src/shaders/compute_test.hlsl
@@ -2,5 +2,5 @@
 
 [numthreads(16, 16, 1)]
 void main(I32x2 i : _globalId) {
-    rwTexture2DfUniform(0)[i] = F32x4((i.x & 3) / 4.0, (i.y & 3) / 4.0, 0, 1);
+    rwTexture2DUniform(0)[i] = F32x4((i.x & 3) / 4.0, (i.y & 3) / 4.0, 0, 1);
 }
